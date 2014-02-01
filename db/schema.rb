f1674dd3df4a08cd3ed4753241af35e744d28bd6
Marco <marco.a.cerna@gmail.com>
2014-01-31 17:48:28 -0800

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140104024552) do
+ActiveRecord::Schema.define(:version => 20140201014648) do
 
   create_table "key_concepts", :force => true do |t|
     t.string   "info"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(:version => 20140104024552) do
     t.float    "ratingAverage"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
